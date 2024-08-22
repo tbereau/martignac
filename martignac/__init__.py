78dc23756783043d6d066d5e0cad3eb3ef359524
Luis Walter <walter@thphys.uni-heidelberg.de>
2024-08-22 11:21:59 +0200

    Load config and env from current working directory

--- martignac/__init__.py
+++ martignac/__init__.py
@@ -3,6 +3,8 @@
 __author__ = """Tristan Bereau"""
 __email__ = "bereau@uni-heidelberg.de"
 
+import os
+from pathlib import Path
 import confuse
 from confuse.core import Configuration
 
@@ -10,10 +12,14 @@ from martignac.logging import logger
 
 __all__ = ["config"]
 
+if os.environ.get('MARTIGNACDIR') is None:
+    os.environ['MARTIGNACDIR'] = str(Path.cwd())
 CONFIG = confuse.Configuration("martignac", __name__)
 LOGGING_LEVEL = "INFO"
 logger.setLevel(LOGGING_LEVEL)
 
+if Path(CONFIG.config_dir()).resolve() == Path(__file__).parent.resolve():
+    logger.warning('You are currently using the default configuration of Martignac.')
 
 def config() -> Configuration:
     return CONFIG
